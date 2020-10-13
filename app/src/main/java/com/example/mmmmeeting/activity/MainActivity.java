9789872f754161ce87f701cc497bebef520e80d4
dmsdk <dmsdk0940@naver.com>
2020-10-13 21:03:16 +0900

    main change

--- app/src/main/java/com/example/mmmmeeting/activity/MainActivity.java
+++ app/src/main/java/com/example/mmmmeeting/activity/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.mmmmeeting.activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 
@@ -17,10 +14,9 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class MainActivity extends BasicActivity implements View.OnClickListener {
+public class MainActivity extends BasicActivity {
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
-    Button btnMyMeeting, btnMyMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,12 +25,6 @@ public class MainActivity extends BasicActivity implements View.OnClickListener 
         setToolbarTitle("우리 지금 만나");
 
         mAuth = FirebaseAuth.getInstance();
-
-        btnMyMeeting = (Button) findViewById(R.id.btn_checkMeeting);
-        btnMyMap = (Button) findViewById(R.id.btn_myMap);
-
-        btnMyMeeting.setOnClickListener(this);
-        btnMyMap.setOnClickListener(this);
     }
 
     //메뉴바 코드
@@ -68,20 +58,6 @@ public class MainActivity extends BasicActivity implements View.OnClickListener 
 
     public void signOut(){
         FirebaseAuth.getInstance().signOut();
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.btn_checkMeeting:
-                myStartActivity(MeetingGridActivity.class);
-                break;
-
-            // 지도 화면으로 이동
-            case R.id.btn_myMap:
-                myStartActivity(MapPlaceActivity.class);
-                break;
-        }
     }
 
     public void revokeAccess() {
