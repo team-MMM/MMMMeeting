294f1e416ab5a0f75bf8dd67bc7fd86837edc82b
juyoung999 <72245176+juyoung999@users.noreply.github.com>
2020-10-30 14:06:31 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/mmmmeeting/decorators/EventDecorator.java
@@ -0,0 +1,41 @@
+package com.example.mmmmeeting.decorators;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+
+import com.example.mmmmeeting.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * Decorate several days with a dot
+ */
+public class EventDecorator implements DayViewDecorator {
+
+    private final Drawable drawable;
+    private int color;
+    private HashSet<CalendarDay> dates;
+
+    public EventDecorator(int color, Collection<CalendarDay> dates, Activity context) {
+        drawable = context.getResources().getDrawable(R.drawable.more);
+        this.color = color;
+        this.dates = new HashSet<>(dates);
+    }
+
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return dates.contains(day);
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.setSelectionDrawable(drawable); // 날짜 테두리 그리기
+        view.addSpan(new DotSpan(5, color)); // 날짜 밑에 점
+    }
+}
