14181281a0f01aa6db8e580a34b3ef5007eeb9a3
dmsdk <dmsdk0940@naver.com>
2020-11-27 15:43:29 +0900

    어디서 버튼분리

--- app/src/main/java/com/example/mmmmeeting/activity/ContentScheduleActivity.java
+++ app/src/main/java/com/example/mmmmeeting/activity/ContentScheduleActivity.java
@@ -17,6 +17,14 @@ import com.example.mmmmeeting.OnScheduleListener;
 import com.example.mmmmeeting.R;
 import com.example.mmmmeeting.ScheduleDeleter;
 import com.example.mmmmeeting.view.ReadScheduleView_new;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
 
 public class ContentScheduleActivity extends BasicActivity implements View.OnClickListener {
     private ScheduleInfo scheduleInfo;
@@ -24,12 +32,15 @@ public class ContentScheduleActivity extends BasicActivity implements View.OnCli
     private ReadScheduleView_new readContentsVIew;
     private LinearLayout contentsLayout;
     Button btn_calendar, btn_place, btn_attendance;
+    Button btn_middle, btn_search, btn_vote;
+    private View content_schedule;
     String code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_schedule);
+        content_schedule=findViewById(R.id.content_schedule);
 
 
         scheduleInfo = (ScheduleInfo) getIntent().getSerializableExtra("scheduleInfo");
@@ -39,11 +50,17 @@ public class ContentScheduleActivity extends BasicActivity implements View.OnCli
         readContentsVIew = findViewById(R.id.readScheduleView_new);
 
         btn_calendar = findViewById(R.id.calendarBtn);
-        btn_place = findViewById(R.id.placeBtn);
         btn_attendance = findViewById(R.id.attendanceBtn);
 
+        btn_middle = findViewById(R.id.middleBtn);
+        btn_search = findViewById(R.id.searchBtn);
+        btn_vote = findViewById(R.id.voteBtn);
+
+        btn_middle.setOnClickListener(this);
+        btn_search.setOnClickListener(this);
+        btn_vote.setOnClickListener(this);
+
         btn_calendar.setOnClickListener(this);
-        btn_place.setOnClickListener(this);
         btn_attendance.setOnClickListener(this);
 
         boardDeleter = new ScheduleDeleter(this);
@@ -57,8 +74,51 @@ public class ContentScheduleActivity extends BasicActivity implements View.OnCli
                 myStartActivity(CalendarActivity.class, scheduleInfo);
                 break;
 
-            case R.id.placeBtn:
-                myStartActivity(PlaceChoiceActivity.class, scheduleInfo);
+            // 중간 지점 찾기
+            case (R.id.middleBtn):
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("meetings").document(code)
+                        .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                // 해당 문서가 존재하는 경우
+                                // document에서 이름이 userID인 필드의 데이터 얻어옴
+                                List users = (List) document.getData().get("userID");
+                                if(users.size()!=1){
+                                    myStartActivity(MiddlePlaceActivity.class, scheduleInfo);
+                                }
+                                else{
+                                    final Snackbar snackbar = Snackbar.make(content_schedule, "모임원이 1명일 때 중간지점을 찾을 수 없어요", Snackbar.LENGTH_INDEFINITE)
+                                            .setActionTextColor(getColor(R.color.colorPrimary));
+                                    snackbar.setAction("확인", new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            snackbar.dismiss();
+                                        }
+                                    });
+                                    snackbar.show();
+                                }
+                            } else {
+                                // 존재하지 않는 문서
+                                Log.d("Attend", "No Document");
+                            }
+                        } else {
+                            Log.d("Attend", "Task Fail : " + task.getException());
+                        }
+
+                    }
+                });
+                break;
+            // 장소 검색
+            case(R.id.searchBtn):
+                myStartActivity(SearchPlaceActivity.class, scheduleInfo);
+                break;
+            // 장소 투표
+            case(R.id.voteBtn):
+                myStartActivity(VoteActivity.class, scheduleInfo);
                 break;
 
             case R.id.attendanceBtn:
