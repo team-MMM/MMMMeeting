cf04e707224199f7ecbfb6dca196e6dd94ea860d
juyoung999 <72245176+juyoung999@users.noreply.github.com>
2020-11-27 16:20:47 +0900

    모임없는 화면 추가

--- app/src/main/java/com/example/mmmmeeting/activity/MainActivity.java
+++ app/src/main/java/com/example/mmmmeeting/activity/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -35,6 +36,8 @@ public class MainActivity extends BasicActivity implements View.OnClickListener 
     private FirebaseFirestore db;
     Button addMeeting;
     Button attendMeeting;
+    TextView text;
+    int check = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +47,7 @@ public class MainActivity extends BasicActivity implements View.OnClickListener 
 
         addMeeting = findViewById(R.id.meetingAdd);
         attendMeeting = findViewById(R.id.meetingAttend);
+        text = findViewById(R.id.text);
 
         addMeeting.setOnClickListener(this);
         attendMeeting.setOnClickListener(this);
@@ -57,7 +61,7 @@ public class MainActivity extends BasicActivity implements View.OnClickListener 
     }
 
     // 모임 목록을 보여줌
-    private void showMeetings(){
+    private void showMeetings() {
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         final ListView gridView = findViewById(R.id.meetingRoomList);
         final MeetingRoomListAdapter adapter = new MeetingRoomListAdapter();
@@ -76,18 +80,25 @@ public class MainActivity extends BasicActivity implements View.OnClickListener 
                         if (task.isSuccessful()) {
                             //모든 document 확인
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                if(document.getData().get("userID").toString().contains(user.getUid())){
+                                if (document.getData().get("userID").toString().contains(user.getUid())) {
                                     // 현재 유저가 존재하는 모임을 찾은 경우
                                     // adapter에 모임에 대한 정보를 갖는 객체 생성해 저장
-                                    adapter.addItem(new MeetingRoomItems(document.getData().get("name").toString(),document.getData().get("description").toString()));
+                                    adapter.addItem(new MeetingRoomItems(document.getData().get("name").toString(), document.getData().get("description").toString()));
+                                    check = 1;
                                     Log.d("Document Read", document.getId() + " => " + document.getData());
                                 } else {
                                     Log.d("Document Snapshot", "No Document");
                                 }
                             }
-                            // 모든 문서 확인이 끝나면
-                            // gridView에 adapter 할당 -> adapter에 있는 아이템 그리드 뷰로 화면에 출력
-                            gridView.setAdapter(adapter);
+                            if (check == 0) {
+                                text.setText("등록된 모임이 없습니다." + "\n" + "모임을 생성하거나 참여해보세요!");
+                            } else {
+                                // 모든 문서 확인이 끝나면
+                                // gridView에 adapter 할당 -> adapter에 있는 아이템 그리드 뷰로 화면에 출력
+                                text.setText("");
+                                gridView.setAdapter(adapter);
+                            }
+
                         } else {
                             Log.d("Document Read", "Error getting documents: ", task.getException());
                         }
@@ -98,28 +109,28 @@ public class MainActivity extends BasicActivity implements View.OnClickListener 
     //메뉴바 코드
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.setting,menu);
+        getMenuInflater().inflate(R.menu.setting, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.revoke:
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle("회원 탈퇴")        // 제목
-                        .setMessage( "우리 지금 만나 앱을 정말로 탈퇴하시겠습니까?")        // 메세지
+                        .setMessage("우리 지금 만나 앱을 정말로 탈퇴하시겠습니까?")        // 메세지
                         // .setCancelable(false)        // 뒤로 버튼 클릭시 취소 가능 설정
-                        .setPositiveButton("확인", new DialogInterface.OnClickListener(){
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
                             // 확인 버튼 클릭시 설정, 오른쪽 버튼입니다.
-                            public void onClick(DialogInterface dialog, int whichButton){
+                            public void onClick(DialogInterface dialog, int whichButton) {
                                 revokeAccess();
                                 finish();
                             }
                         })
-                        .setNegativeButton("취소", new DialogInterface.OnClickListener(){// 취소 버튼 클릭시
-                            public void onClick(DialogInterface dialog, int whichButton){//취소 이벤트...
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {// 취소 버튼 클릭시
+                            public void onClick(DialogInterface dialog, int whichButton) {//취소 이벤트...
                             }
                         });
                 AlertDialog dialog = builder.create();    // 알림창 객체 생성
@@ -142,7 +153,7 @@ public class MainActivity extends BasicActivity implements View.OnClickListener 
 
 
     // 로그아웃 함수
-    private void signOut(){
+    private void signOut() {
         FirebaseAuth.getInstance().signOut();
     }
 
@@ -159,7 +170,7 @@ public class MainActivity extends BasicActivity implements View.OnClickListener 
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        Log.d("revoke User","DocumentSnapshot successfully deleted!");
+                        Log.d("revoke User", "DocumentSnapshot successfully deleted!");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -178,10 +189,10 @@ public class MainActivity extends BasicActivity implements View.OnClickListener 
                             //모든 document 확인
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 // 현재 uid가 모임 정보에 존재하는 경우
-                                if(document.getData().get("userID").toString().contains(mAuth.getUid())){
+                                if (document.getData().get("userID").toString().contains(mAuth.getUid())) {
                                     // db에서 현재 유저 uid 삭제
                                     DocumentReference userdel = db.collection("meetings").document(document.getId());
-                                    if(document.get("leader").equals(mAuth.getUid())){
+                                    if (document.get("leader").equals(mAuth.getUid())) {
                                         userdel.update("leader", "");
                                     }
                                     userdel.update("userID", FieldValue.arrayRemove(mAuth.getUid()));
@@ -216,12 +227,12 @@ public class MainActivity extends BasicActivity implements View.OnClickListener 
                     Log.d("Delete3", document.get("userID").toString());
                     // 모임원 없는 모임이 된 경우 모임 삭제
 
-                    Log.d("Delete3 Test", "users:"+(document.get("userID").toString().length()));
+                    Log.d("Delete3 Test", "users:" + (document.get("userID").toString().length()));
                     // 모임원 없음 = 2
                     // 모임원 한 명 = 30
                     // 이 후 모임원 한 명 증가시마다 length +30 씩 증가
 
-                    if (document.get("userID").toString().length()==2) {
+                    if (document.get("userID").toString().length() == 2) {
                         // 모임에 있는 약속, 게시판 내용도 전부 삭제해야..
                         meetingdel.delete();
                     }
@@ -233,19 +244,19 @@ public class MainActivity extends BasicActivity implements View.OnClickListener 
     }
 
 
-    private void myStartActivity(Class c){
-        Intent intent = new Intent(this,c);
+    private void myStartActivity(Class c) {
+        Intent intent = new Intent(this, c);
         intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case (R.id.meetingAdd):
                 myStartActivity(MakeMeetingActivity.class);
                 break;
-            case(R.id.meetingAttend):
+            case (R.id.meetingAttend):
                 myStartActivity(MeetingAttendActivity.class);
                 break;
         }
